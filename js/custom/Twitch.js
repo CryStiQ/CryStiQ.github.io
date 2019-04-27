3b292ba39898188616aecae6e714efb92a62a92d
Mednis <reinis@mednis.id.lv>
2019-04-28 02:56:42 +0300

    Added twitch API to monitor live status of twitch channel!
    Minor cleanup in main index.html to make it more readable!

---
+++ js/custom/Twitch.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){ 											// Runs on page load!
+	$.getJSON('https://api.twitch.tv/kraken/streams/crystiq?client_id=6di40nucwiqexur68xkayh0gxmc6gp', function(channeldata){
+		if (channeldata["stream"] !== null) { 
+        	$("#twitch-status").html("<i class='far fa-circle'></i> Live for " + channeldata["stream"].viewers + " viewers!");
+		}
+	})
+}); 
