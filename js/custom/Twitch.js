dac73266ad9791390f222dc6adcf9a4053ed7645
CryStiQ <43320307+CryStiQ@users.noreply.github.com>
2019-04-29 12:33:22 +0300

    Update Twitch.js

--- js/custom/Twitch.js
+++ js/custom/Twitch.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){ 											// Runs on page load!
 	$.getJSON('https://api.twitch.tv/kraken/streams/crystiq?client_id=6di40nucwiqexur68xkayh0gxmc6gp', function(channeldata){
 		if (channeldata["stream"] !== null) { 
-        	$("#twitch-status").html("<i class='fa fa-circle'></i> Live for " + channeldata["stream"].viewers + " viewers!");
-		}
+        		$("#twitch-status").html("<i class='fa fa-circle'></i> Live for " + channeldata["stream"].viewers + " viewers!");
+		} else {
+			$("#twitch-status").text("Offline!");
+		};
 	})
 }); 
