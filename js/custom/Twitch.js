3bb479ee59ac6eec5591b13ecba13091e2f7b097
Mednis <reinis@mednis.id.lv>
2019-04-30 20:25:35 +0300

    Twitch button live badge update!
    The twitch live badge now changes it's colours depending on stream status!
    No more boring gray badges!
    
    Also, general code readability and commenting improvements for the twitch javascript file!

--- js/custom/Twitch.js
+++ js/custom/Twitch.js
@@ -1,9 +1,20 @@
-$(document).ready(function(){ 											// Runs on page load!
-	$.getJSON('https://api.twitch.tv/kraken/streams/crystiq?client_id=6di40nucwiqexur68xkayh0gxmc6gp', function(channeldata){
+/** 
+Gets data from the twitch API in order to determine the stream status,
+shows current stream status in a badge icon for the twitch button!
+**/
+
+const TwitchAPI = "https://api.twitch.tv/kraken/streams/crystiq?client_id=6di40nucwiqexur68xkayh0gxmc6gp"; // Twitch API url!
+
+$(document).ready(function(){ // Runs on page load!
+	$.getJSON(TwitchAPI, function(channeldata){
 		if (channeldata["stream"] !== null) { 
-        		$("#twitch-status").html("<i class='fa fa-circle'></i> Live for " + channeldata["stream"].viewers + " viewers!");
-		} else {
-			$("#twitch-status").text("Offline!");
+			// The stream is not null, AKA it's live!
+        	$("#twitch-status").addClass('badge-twitch-live').html("<i class='fa fa-circle'></i> Live for " + channeldata["stream"].viewers + " viewers!"); // Shows the current viewers, sets badge style to live red color!
+		
+		} else { 
+			// The stream is most likely offline!
+
+			$("#twitch-status").addClass('badge-twitch-offline').text("Off air!") // Sets the badge style and text!
 		};
 	})
 }); 
